a2d488afe5e9155eef0471c0ec96f85c29cbae41
Michael Kox <git1@michael-kox.de>
2024-06-15 11:37:53 +0200

    Code to get actor lists is moved to new file.

--- controllers/actorController.js
+++ controllers/actorController.js
@@ -4,7 +4,8 @@ const FollowedAtpActor = require("../models/followed-atp-actor");
 const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
 
-const {getAllFollowers, getFollowedActor} = require("../lib/actor");
+const { getAllFollowers, getFollowedActor } = require("../lib/actor");
+const { getActorLists } = require("../lib/actor-from-mongodb");
 
 exports.index = asyncHandler(async (req, res, next) => {
   res.render("index", {
@@ -29,31 +30,12 @@ exports.actor_list_get = asyncHandler(async (req, res, next) => {
 
 // Display list of actors.
 exports.actor_list_post = asyncHandler(async (req, res, next) => {
-  var keywordsearch = { did: { $exists: true } };
-  if (req.body.keywords != '') {
-   keywordsearch = { $text: { $search: req.body.keywords } };
-  }
-console.log('req.body: ', req.body);
-  var numberOfActors = req.body.how_much_random_actors;
-  var sampleSize = parseInt(numberOfActors) * 10;
-
-  const [followedAtpActors, actors] = await Promise.all([
-    FollowedAtpActor.find({})
-      .sort({ displayName: 1 })
-      .exec(),
-    AtpActor.aggregate([
-      { $match : keywordsearch }, // $text match only works, when on first place
-      { $sample: { size: sampleSize } }
-    ])
-  ]);
-
-  var actorsFinal = actors.slice(0, parseInt(numberOfActors));
+  var actorLists = await getActorLists(req.body);
 
   res.render("actor_list", { 
     title: "Actor List", 
-    followed_actors: 
-    followedAtpActors, 
-    actor_list: actorsFinal,
+    followed_actors: actorLists.followedAtpActors, 
+    actor_list: actorLists.actorsFinal,
     keywords: req.body.keywords,
     numberOfActors: req.body.how_much_random_actors,
     howMuchActorSitesTogether: req.body.how_much_actor_sites_together
